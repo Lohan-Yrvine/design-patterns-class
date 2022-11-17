e08b9c684da91008156403c34e67761561fdb6ea
Lohan Pinheiro <o.lohan.yrvine@gmail.com>
2022-11-17 17:11:47 -0300

    chain of responsibility little fix at main

--- chain-of-responsibility/bin/main.dart
+++ chain-of-responsibility/bin/main.dart
@@ -1,13 +1,14 @@
 import 'dart:io';
 
+import 'package:chain_of_responsibility/handler.dart';
 import 'package:chain_of_responsibility/space_handler.dart';
 import 'package:chain_of_responsibility/letter_a_handler.dart';
 import 'package:chain_of_responsibility/dot_handler.dart';
 
 void main() {
-  final dotsHandler = DotsHandler();
-  final letterAHandler = LetterAHandler(dotsHandler);
-  final spacesHandler = SpacesHandler(letterAHandler);
+  final Handler dotsHandler = DotsHandler();
+  final Handler letterAHandler = LetterAHandler(dotsHandler);
+  final Handler spacesHandler = SpacesHandler(letterAHandler);
 
   final request = stdin.readLineSync();
   spacesHandler.handleRequest(request!);
